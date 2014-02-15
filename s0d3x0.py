38db6248c24d3a8cb1eb313fa7007bbe85ad2698
Hari K <hari01234@gmail.com>
2014-02-15 09:42:48 +0530

    Put major part of the arithmetic inside a loop

--- s0d3x0.py
+++ s0d3x0.py
@@ -34,52 +34,26 @@ group.add_argument("--display",help = "Displays the remaining coupons",action = 
 group.add_argument("bill",nargs = '?',help = "Total bill amount",type = int)
 args = parser.parse_args()
 
+denom = [50,35,20,10]
+rem = [s._50,s._35,s._20,s._10]
+
 if args.bill is not None:
     f = open("dump","w")
-
-    n = args.bill/50
-    if n <= s._50:
-        print "{:^3}{:^3}{:^3}".format(n,'x',"50")
-        f.write("{}{}".format(s._50-n,"\n"))
-        args.bill %= 50
-    else:
-        print "{:^3}{:^3}{:^3}".format(s._50,'x',"50")
-	f.write("{}{}".format(0,"\n"))
-	args.bill -= s._50*50
-
-    n = args.bill/35
-    if n <= s._35:
-        print "{:^3}{:^3}{:^3}".format(n,'x',"35")
-        f.write("{}{}".format(s._35-n,"\n"))
-	args.bill %= 35
-    else:
-	print "{:^3}{:^3}{:^3}".format(s._35,'x',"35")
-	f.write("{}{}".format(0,"\n"))
-	args.bill -= s._35*35
-
-    n = args.bill/20
-    if n <= s._20:
-        print "{:^3}{:^3}{:^3}".format(n,'x',"20")
-        f.write("{}{}".format(s._20-n,"\n"))
-	args.bill %= 20
-    else:
-	print "{:^3}{:^3}{:^3}".format(s._20,'x',"20")
-	f.write("{}{}".format(0,"\n"))
-	args.bill -= s._20*20
-
-    n = args.bill/10
-    if n <= s._10:
-        print "{:^3}{:^3}{:^3}".format(n,'x',"10")
-        f.write("{}{}".format(s._10-n,"\n"))
-	args.bill %= 10
-    else:
-	print "{:^3}{:^3}{:^3}".format(s._10,'x',"10")
-	f.write("{}{}".format(0,"\n"))
-	args.bill -= s._10*10
-
-    print "Rs.{:<5} to be paid in cash.".format(args.bill)
+    
+    for i in range(4):
+        n = args.bill/denom[i]
+        if n <= rem[i]:
+            print "{:^3}{:^3}{:^3}".format(n,'x',denom[i])
+            f.write("{}{}".format(rem[i]-n,"\n"))
+            args.bill %= denom[i]
+        else:
+            print "{:^3}{:^3}{:^3}".format(rem[i],'x',denom[i])
+	    f.write("{}{}".format(0,"\n"))
+	    args.bill -= rem[i]*denom[i]    
+    
+    print "Rs.{:<3} to be paid in cash.".format(args.bill)
     f.close()
-
+    
 elif args.addBook is True:
     f = open("dump","w")
     s._50 += 10
