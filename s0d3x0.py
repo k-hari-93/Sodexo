3ede5c0189cd560e902cf4130985a5edde2cdcec
Hari K <hari01234@gmail.com>
2014-02-15 09:22:34 +0530

    Added A Mutually Exclusive Group For Argparse

--- s0d3x0.py
+++ s0d3x0.py
@@ -5,7 +5,7 @@
 #10 coupons of Rs.10
 
 
-import argparse,sys
+import argparse
 
 class S0d3x0(object):
     def __init__(self):
@@ -25,19 +25,14 @@ class S0d3x0(object):
             self._10 = 0
 
 s = S0d3x0()
-print "\n"*2
+print "\n"
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--addBook",help = "Adds a CouponBook",action = "store_true")
-parser.add_argument("--display",help = "Displays the remaining coupons",action = "store_true")
-parser.add_argument("bill",nargs = '?',help = "Total bill amount",type = int)
+group = parser.add_mutually_exclusive_group(required = True)
+group.add_argument("--addBook",help = "Adds a CouponBook",action = "store_true")
+group.add_argument("--display",help = "Displays the remaining coupons",action = "store_true")
+group.add_argument("bill",nargs = '?',help = "Total bill amount",type = int)
 args = parser.parse_args()
-
-try:
-    assert len(sys.argv)<4 and len(sys.argv)>1
-except AssertionError:
-    print "ArgumentError: Type 'python s0d3x0.py -h' for more details"
-    sys.exit()
 
 if args.bill is not None:
     f = open("dump","w")
@@ -103,4 +98,4 @@ else:
     print "{:^15}{:^15}{:^15}{:^15}".format("Fifty","Thirty-five","Twenty","Ten")
     print "{:^15}{:^15}{:^15}{:^15}".format(s._50,s._35,s._20,s._10)
         
-print "\n"*2
+print "\n"
